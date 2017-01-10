5958d941ef171d668c0cf39db5dd7a374cc0845d
Michael Tran <koramaiku@Michaels-iMac.local>
2017-01-11 00:33:22 +1100

    Finished adding the Artist Component, added models and further code

--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
+import { Artist } from '../../models/Artist';
 
-@Component({
+@Component({  
   moduleId: module.id,
   selector: 'search',
   templateUrl: 'search.component.html',
@@ -8,8 +10,16 @@ import { Component } from '@angular/core';
 
 export class SearchComponent { 
   searchStr:string;
+  searchRes: Artist[];
+
+  constructor(private _spotifyService:SpotifyService){
+
+  }
   
   searchMusic(){
-    console.log(this.searchStr);
+    this._spotifyService.searchMusic(this.searchStr).subscribe(res => {
+      console.log(res.artists.items);
+      this.searchRes = res.artists.items;
+    });
   }
 }
